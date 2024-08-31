dbd88be8817c8af5c0da27d5727127bd9c06879a
Rohan Chaudhry <rohan.chaudhry97@gmail.com>
2024-08-30 23:58:01 -0500

    Initial Commit

---
+++ Haikus/HaikusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaikusApp.swift
+//  Haikus
+//
+//  Created by Rohan Chaudhry on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct HaikusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
